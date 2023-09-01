d0cf4d1e484a8602ecc0c61dd3f5258d09dbd01e
venom <abdo30201080101694@gmail.com>
2023-09-01 15:29:33 +0300

    to video 52

---
+++ models/subCategoryModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const subCategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      trim: true,
+      unique: [true, "This subCategory is already exist"],
+      minlength: [2, "too short name"],
+      maxlength: [30, "too long name"],
+      required: true,
+    },
+    slug: {
+      type: String,
+      lowercase: true,
+    },
+    category: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Category",
+      required: [true, "The parent category required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("SubCategory", subCategorySchema);
